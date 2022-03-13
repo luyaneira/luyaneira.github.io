0cc9bc63ba54e4059f1f8d3fb2de1123fc9eca31
Nancy Meike <nancy.meike@deutschebahn.com>
2022-03-13 20:03:06 +0100

    feedback and survey

---
+++ digitalSummit/script.js
@@ -0,0 +1,33 @@
+var currentPopup = undefined;
+var isCoWebSiteOpened =  false;
+var urlFeedback = "https://forms.office.com/Pages/ResponsePage.aspx?id=nC2noeZJbU-a9lqvoRg7_f26WHDvlOFNi_8Y43fECOdUMDVDTUpUUDRONkxHMzdLQ09WRlQxUUZSMS4u";
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+var zoneFeedback = "feedback";
+
+WA.room.onEnterZone(zoneFeedback, () => {
+   currentPopup =  WA.ui.openPopup("popUpFeedback","Hier kannst du Feedback abgeben.",[
+        {
+            label: "Feedback",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urlFeedback);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneFeedback, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
