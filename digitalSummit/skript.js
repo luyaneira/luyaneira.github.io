0212d8b1541bef73cbe3196b9e4de916facce07c
Nancy Meike <nancy.meike@deutschebahn.com>
2022-03-14 21:29:36 +0100

    skript update

--- digitalSummit/skript.js
+++ digitalSummit/skript.js
@@ -1,3 +1,5 @@
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
 var currentPopup = undefined;
 var isCoWebSiteOpened =  false;
 var urlTutorial = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure-erste-schritte";
@@ -12,11 +14,32 @@ function closePopUp(){
 var zoneIntro = "intro";
 var zoneTutorial = "tutorial";
 
+WA.room.onEnterZone(zoneTutorial, () => {
+    currentPopup = WA.ui.openPopup("popUpTutorial", "Möchtest du dir das Tutorial ansehen?.",[
+         {
+             label: "Tutorial",
+             callback: (popup => {
+                 WA.nav.openCoWebSite(urlTutorial);
+                 isCoWebSiteOpened = true;
+                 closePopUp();
+             })
+         }]);
+ })
+ 
+ WA.room.onLeaveZone(zoneTutorial, () =>{
+     closePopUp();
+ 
+     if (isCoWebSiteOpened) {
+         WA.nav.closeCoWebSite();
+         isCoWebSiteOpened = false;
+     }
+ })
+
 WA.room.onEnterZone(zoneIntro, () => {
     currentPopup = WA.ui.openPopup("popUpIntro", "Willkommen beim DigitalSummit in WorkAdventure!\n" +
     "Zur Bewegung verwenden Sie die Pfeiltasten oder navigieren per Rechtsklick zu einem Punkt auf der Karte.\n" +
     "Wenn Sie ein ausführliches Tutorial anschauen möchten können Sie zur Tutorial-Tafel vor dem Bahnhofsgebäude laufen.\n" +
-    "Wir wünschen Ihnen viel Spaß beim DigitalSummit im WorkAdventure!", [
+    "Wir wünschen Ihnen viel Spaß beim DigitalSummit in WorkAdventure!", [
         {
             label: "Alles Klar!",
             callback: (popup => {
@@ -27,25 +50,4 @@ WA.room.onEnterZone(zoneIntro, () => {
 
 WA.room.onLeaveZone(zoneIntro, () => {
     closePopUp();
-})
-
-WA.room.onEnterZone(zoneTutorial, () => {
-   currentPopup = WA.ui.openPopup("popUpTutorial", "Möchtest du dir das Tutorial ansehen?.",[
-        {
-            label: "Tutorial",
-            callback: (popup => {
-                WA.nav.openCoWebSite(urlTutorial);
-                isCoWebSiteOpened = true;
-                closePopUp();
-            })
-        }]);
-})
-
-WA.room.onLeaveZone(zoneTutorial, () =>{
-    closePopUp();
-
-    if (isCoWebSiteOpened) {
-        WA.nav.closeCoWebSite();
-        isCoWebSiteOpened = false;
-    }
 })
