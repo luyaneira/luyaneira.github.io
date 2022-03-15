eec2c5812ccaeaff8b8e7a4894646032fe8e2138
Nancy Meike <nancy.meike@deutschebahn.com>
2022-03-15 10:17:26 +0100

    food links

--- digitalSummit/skript.js
+++ digitalSummit/skript.js
@@ -11,49 +11,109 @@ function closePopUp(){
 
 var zoneIntro = "intro";
 var zoneTutorial = "tutorial";
+var zoneTutorial1 = "tutorial1";
+var zoneTutorial2 = "tutorial2";
 var zoneGuide = "guide";
+var zoneGuide1 = "guide1";
+var zoneGuide2 = "guide2";
+
+var guideMsg = "Wegweiser\n\n" +
+"Hauptbahnhof (Norden): Stiller Bereich\n" +
+"Dicker Bulle (Nordwesten): Worms\n" +
+"Green Pub (Nordosten): Pong\n" +
+"Marktstand (Zentral): Treffpunkt\n" +
+"Cocktailbar (Südwesten): Treffpunkt\n" +
+"Dancehall (Südosten): Musik\n" +
+"Silberturm (Süden): Minirätsel\n"
+
+var tutorialMsg = "Möchtest du dir das Tutorial ansehen?"
 
 WA.room.onEnterZone(zoneTutorial, () => {
-    currentPopup = WA.ui.openPopup("popUpTutorial", "Möchtest du dir das Tutorial ansehen?.",[
-         {
-             label: "Tutorial",
-             callback: (popup => {
-                 WA.nav.openCoWebSite(urlTutorial);
-                 isCoWebSiteOpened = true;
-                 closePopUp();
-             })
-         }]);
- })
- 
- WA.room.onLeaveZone(zoneTutorial, () =>{
-     closePopUp();
- 
-     if (isCoWebSiteOpened) {
-         WA.nav.closeCoWebSite();
-         isCoWebSiteOpened = false;
-     }
- })
+    currentPopup = WA.ui.openPopup("popUpTutorial", tutorialMsg, [
+        {
+            label: "Nein",
+            callback: (popup => {
+                isCoWebSiteOpened = false;
+                closePopUp();
+            })
+        },
+        {
+            label: "Ja",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urlTutorial);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }
+    ]);
+})
 
- WA.room.onEnterZone(zoneGuide, () => {
-    currentPopup = WA.ui.openPopup("popUpGuide", "Wegweiser\n\n" +
-    "Hauptbahnhof: Ankunftspunkt (Stiller Bereich)\n" +
-    "Dicker Bulle: Treffpunkt für Worms (Spiel)\n" +
-    "Green Pub: Treffpunkt für Pong (Spiel)\n" +
-    "Marktstand: Treffpunkt (Gespräche)\n" +
-    "Cocktailbar: Treffpunkt (Gespräche und Cocktails)\n" +
-    "Dancehall: Treffpunkt (Musik)\n" +
-    "Silberturm: Treffpunkt (Rätsel)\n" ,[
-         {
-             label: "OK",
-             callback: (popup => {
-                 closePopUp();
-             })
-         }]);
- })
+WA.room.onEnterZone(zoneTutorial1, () => {
+    currentPopup = WA.ui.openPopup("popUpTutorial1", tutorialMsg, [
+        {
+            label: "Nein",
+            callback: (popup => {
+                isCoWebSiteOpened = false;
+                closePopUp();
+            })
+        },
+        {
+            label: "Ja",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urlTutorial);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }
+    ]);
+})
+
+WA.room.onEnterZone(zoneTutorial2, () => {
+    currentPopup = WA.ui.openPopup("popUpTutorial2", tutorialMsg, [
+        {
+            label: "Nein",
+            callback: (popup => {
+                isCoWebSiteOpened = false;
+                closePopUp();
+            })
+        },
+        {
+            label: "Ja",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urlTutorial);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }
+    ]);
+})
  
- WA.room.onLeaveZone(zoneGuide, () =>{
-     closePopUp();
- })
+WA.room.onLeaveZone(zoneTutorial, () =>{
+    closePopUp();
+ 
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onLeaveZone(zoneTutorial1, () =>{
+    closePopUp();
+ 
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onLeaveZone(zoneTutorial2, () =>{
+    closePopUp();
+ 
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
 
 WA.room.onEnterZone(zoneIntro, () => {
     currentPopup = WA.ui.openPopup("popUpIntro", "Willkommen beim DigitalSummit in WorkAdventure!\n" +
@@ -69,5 +129,47 @@ WA.room.onEnterZone(zoneIntro, () => {
 })
 
 WA.room.onLeaveZone(zoneIntro, () => {
+    closePopUp();
+})
+
+WA.room.onEnterZone(zoneGuide, () => {
+    currentPopup = WA.ui.openPopup("popUpGuide", guideMsg, [
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]);
+ })
+
+WA.room.onEnterZone(zoneGuide1, () => {
+    currentPopup = WA.ui.openPopup("popUpGuide1", guideMsg, [
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onEnterZone(zoneGuide2, () => {
+    currentPopup = WA.ui.openPopup("popUpGuide2", guideMsg, [
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]);
+})
+ 
+WA.room.onLeaveZone(zoneGuide, () =>{
+    closePopUp();
+})
+
+WA.room.onLeaveZone(zoneGuide1, () =>{
+    closePopUp();
+})
+
+WA.room.onLeaveZone(zoneGuide2, () =>{
     closePopUp();
 })
