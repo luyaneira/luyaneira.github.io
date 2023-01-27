d327a7d78818c48035e0a07fcd1b2d47228e5663
Nancy Meike <nancy.meike@deutschebahn.com>
2023-01-27 15:54:38 +0100

    grouped configuration

--- skydeck/src/skydeck_ffm.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-// Calling bootstrapExtra will initiliaze all the "custom properties"  
-bootstrapExtra();
-
-console.log('Script started successfully');
