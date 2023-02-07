be2ffc3b883b423d84c0123bb2f14a11a6280ad3
Nancy Meike <nancy.meike@deutschebahn.com>
2023-02-07 10:56:45 +0100

    added round walls

---
+++ skydeck/src/skydeck_ffm.ts
@@ -0,0 +1,7 @@
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+// Calling bootstrapExtra will initiliaze all the "custom properties"  
+bootstrapExtra();
+
+
+console.log('Script started successfully');
